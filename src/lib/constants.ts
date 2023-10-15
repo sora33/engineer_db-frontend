438d268e66d99011dcce14233cf53d6ca3db933d
hiranuma <shuuuya0616@gmail.com>
2023-10-16 08:37:32 +0900

    汎用的なutilを実装

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const NavList = [
+  { name: "サービス", href: "/service" },
+  { name: "提携情報", href: "/partner" },
+  { name: "お知らせ", href: "/news" },
+  { name: "ブログ", href: "/blog" },
+  { name: "採用情報", href: "/recruit" },
+  { name: "お問合わせ", href: "/contact" },
+  { name: "プライバシーポリシー", href: "/policy" },
+];
+export const NavAboutList = [
+  { name: "私たちの価値観", href: "/about/values" },
+  { name: "代表挨拶", href: "/about/greeting" },
+];
+
+export const FooterNavList = [, ...NavAboutList, ...NavList];
---
+++ src/lib/customAxios.ts
@@ -0,0 +1,48 @@
+import { NextRequest } from "next/server";
+import axios from "axios";
+import applyCaseMiddleware from "axios-case-converter";
+import { getToken } from "next-auth/jwt";
+
+const options = {
+  ignoreHeaders: true,
+};
+
+export const customAxios = applyCaseMiddleware(
+  axios.create({
+    baseURL: process.env.BACKEND_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }),
+  options
+);
+
+export async function getJWEToken(req: NextRequest) {
+  const secret = process.env.NEXTAUTH_SECRET;
+  return await getToken({ req, secret, raw: true });
+}
+
+customAxios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error);
+    switch (error.response.status) {
+      case 400:
+        throw error;
+      case 401:
+        alert("ログインしてください");
+        break;
+      case 403:
+        alert("権限がありません。");
+        break;
+      case 404:
+        break;
+      case 500:
+        console.log("サーバー内部エラーが発生しました");
+        break;
+      default:
+        console.log("予期しないエラーが発生しました");
+        break;
+    }
+  }
+);
