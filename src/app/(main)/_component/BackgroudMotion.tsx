5e84be48a112612ba9d6a72255714b197edf1a8c
hiranuma <shuuuya0616@gmail.com>
2023-10-16 09:20:23 +0900

    デザインの微修正

--- src/app/(main)/_component/BackgroudMotion.tsx
+++ src/app/(main)/_component/BackgroudMotion.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export const generateRandomValue = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
@@ -9,14 +10,19 @@ const generateRandomColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  return `rgba(${r},${g},${b},0.05)`; // Semi-transparent color
+  return `rgba(${r},${g},${b},0.05)`;
 };
 
 type props = {
   index: number;
 };
 export const BackgroudMotion: React.FC<props> = ({ index }) => {
-  const randomSize = generateRandomValue(1, (window.innerWidth * 0.2) / 16);
+  const [size, setSize] = useState<number>(0);
+
+  useEffect(() => {
+    const newSize = generateRandomValue(1, (window.innerWidth * 0.3) / 16);
+    setSize(newSize);
+  }, []);
   const randomX = generateRandomValue(0, 100);
   const randomY = generateRandomValue(0, 100);
   const randomDuration = generateRandomValue(1, 10);
@@ -24,13 +30,13 @@ export const BackgroudMotion: React.FC<props> = ({ index }) => {
 
   return (
     <motion.div
-      className="fixed rounded-full"
+      className="fixed -z-10 rounded-full"
       style={{
         backgroundColor: randomColor,
         top: `${randomY}%`,
         left: `${randomX}%`,
-        width: `${randomSize}rem`,
-        height: `${randomSize}rem`,
+        width: `${size}rem`,
+        height: `${size}rem`,
       }}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
