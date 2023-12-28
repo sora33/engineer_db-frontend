70341b5f3649bdd94027619cee565b205ee0fd15
hiranuma <shuuuya0616@gmail.com>
2023-12-29 01:59:42 +0900

    NextAuthProviderのファイル名を変更

--- src/providers/NextAuth.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { SessionProvider } from 'next-auth/react';
-import { ReactNode } from 'react';
-
-export const NextAuthProvider = ({ children }: { children: ReactNode }) => {
-	return <SessionProvider>{children}</SessionProvider>;
-};
