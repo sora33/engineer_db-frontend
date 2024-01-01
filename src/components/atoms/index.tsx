ba8436242d829afbdb4f15978efcf9e3cdc6614b
hiranuma <shuuuya0616@gmail.com>
2024-01-01 18:27:20 +0900

    atomicレベルのコンポーネントを追加、修正

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -3,6 +3,7 @@ export { Heading } from "@/components/atoms/heading";
 export { Inner } from "@/components/atoms/inner";
 export { LinkText } from "@/components/atoms/link/LinkText";
 export { Span } from "@/components/atoms/span";
+export { Loading } from "@/components/atoms/loading";
 export { Button } from "@/components/atoms/button";
 export { default as Link } from "next/link";
 export { default as Image } from "next/image";
