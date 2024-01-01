042bf993e697e1fa41987e5d44f8c908f8be8cd7
hiranuma <shuuuya0616@gmail.com>
2024-01-01 20:45:02 +0900

    ログインしてなければloginページにリダイレクト

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,10 +1,16 @@
 import { MainTabs } from "@/app/(authenticated)/_component/MainTabs";
 import { LocalStorage } from "@/app/(authenticated)/_component/LocalStorage";
-export default function MainLayout({
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <>
       <LocalStorage />
