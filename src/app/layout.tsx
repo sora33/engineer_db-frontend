d050f347de95ec7567f7efdaad12274e8aef7b97
hiranuma <shuuuya0616@gmail.com>
2024-01-05 16:13:09 +0900

    opgを追加実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,30 @@ export const metadata: Metadata = {
   },
   description:
     "「エンジニアDB」は、エンジニアがサービスに登録しているだけで、企業やエンジニアとつながる仕組みを目指しています。エンジニアデータを集約し、データを民主化することで、多くのエンジニアがつながれる環境を提供します。",
+  openGraph: {
+    title: {
+      default: "エンジニアDB",
+      template: "%s | エンジニアDB",
+    },
+    description:
+      "「エンジニアDB」は、エンジニアがサービスに登録しているだけで、企業やエンジニアとつながる仕組みを目指しています。エンジニアデータを集約し、データを民主化することで、多くのエンジニアがつながれる環境を提供します。",
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    siteName: "エンジニアDB",
+    images: [
+      {
+        url: "/img/logo_opg.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  generator: "Next.js",
+  keywords: ["エンジニア", "データベース", "DX", "マッチング"],
+  authors: [{ name: "平沼" }],
+  creator: "ひらぬま",
+  themeColor: "F97316",
 };
 
 export default function RootLayout({
