16b21f954f9d815eec703b311f62d3ce3422551b
hiranuma <shuuuya0616@gmail.com>
2024-01-06 23:00:58 +0900

    JestのtimeZoneを"Asia/Tokyo",に変更

--- jest.config.ts
+++ jest.config.ts
@@ -17,7 +17,7 @@ const customJestConfig = {
     "@/(.*)$": "<rootDir>/src/$1",
   },
   testEnvironmentOptions: {
-    timeZone: "JST",
+    timeZone: "Asia/Tokyo",
   },
 };
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
