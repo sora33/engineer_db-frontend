d6f8c8d572a513235c2f9ad6b07bc40ef1972410
hiranuma <shuuuya0616@gmail.com>
2024-01-06 16:53:15 +0900

    不要なファイルを削除

--- src/components/animation/index.tsx
+++
@@ -1,3 +0,0 @@
-export { MotionWhileInView } from "@/components/animation/MotionWhileInView";
-export { MotionUpDown } from "@/components/animation/MotionUpDown";
-export { MotionDrag } from "@/components/animation/MotionDrag";
