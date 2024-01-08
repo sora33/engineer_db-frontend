54d9cde7cd623d571d2325a9e2b8b0b9dcad3432
hiranuma <shuuuya0616@gmail.com>
2024-01-08 15:16:08 +0900

    プログレスバーを実装

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,8 +1,7 @@
 import { Inner } from "@/components/atoms/inner";
 import { Logo } from "@/components/layout/header/Logo";
 import { Nav } from "@/components/layout/header/nav";
-import Link from "next/link";
-
+import { Link } from "@/components/atoms";
 export const Header = () => {
   return (
     <header className="bg-white py-2 md:py-4">
