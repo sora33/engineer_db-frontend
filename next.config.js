284d4b69618a8355b6bf9b9c1ffb1eb6be79ecc4
Hiranuma Shuya(ひらぬま) <shuuuya0616@gmail.com>
2024-11-03 00:07:03 +0900

    リダイレクト

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/:path*",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
