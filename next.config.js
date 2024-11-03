0a3ce77882d87c1b09e0b6cc114dcaf7d017892d
Hiranuma Shuya(ひらぬま) <shuuuya0616@gmail.com>
2024-11-03 11:29:51 +0900

    リダイレクト設定

--- next.config.js
+++ next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {
   // 現在はリダイレクトを無効化しています。
   // 全てのパスを "/"にリダイレクトすると、
   // 他のページにアクセスできなくなってしまうためです。
-  // redirects: async () => {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       destination: "/",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  redirects: async () => {
+    return [
+      {
+        source: "/:path*",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
